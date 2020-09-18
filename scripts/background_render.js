efe207b720dafb5f0a434ffa873ae7176ef6c25a
HedonisticOpportunist <fujitahibaraki@gmail.com>
2020-09-18 12:56:54 +0100

    Add files via upload

--- scripts/background_render.js
+++ scripts/background_render.js
@@ -3,34 +3,34 @@
 // ---------------------------
 //Function to draw cloud objects.
 function drawClouds() {
-	// Draw clouds.
-	for (var i = 0; i < clouds.length; i++) {
-		fill("#b2dffb");
-		noStroke();
+  // Draw clouds.
+  for (var i = 0; i < clouds.length; i++) {
+    fill("#b2dffb");
+    noStroke();
 
-		ellipse(clouds[i].x_pos, clouds[i].y_pos, clouds[i].width, clouds[i].height);
-	}
+    ellipse(clouds[i].x_pos, clouds[i].y_pos, clouds[i].width, clouds[i].height);
+  }
 }
 
 //Function to draw mountains objects.
 function drawMountains() {
-	for (var i = 0; i < mountains.length; i++) {
-		noStroke();
-		fill("#fffafa");
+  for (var i = 0; i < mountains.length; i++) {
+    noStroke();
+    fill("#fffafa");
 
-		triangle(mountains[i].x_pos, mountains[i].y_pos, mountains[i].x_pos + 150, mountains[i].y_pos - 415, mountains[i].x_pos + 350, mountains[i].y_pos);
-	}
+    triangle(mountains[i].x_pos, mountains[i].y_pos, mountains[i].x_pos + 150, mountains[i].y_pos - 415, mountains[i].x_pos + 350, mountains[i].y_pos);
+  }
 }
 
 //Function to draw trees objects.
 function drawTrees() {
-	for (var i = 0; i < trees.length; i++) {
-		fill("#e8ded2");
-		rect(trees[i].tree_pos_x, tree_pos_y, 50, 235);
+  for (var i = 0; i < trees.length; i++) {
+    fill("#e8ded2");
+    rect(trees[i].tree_pos_x, tree_pos_y, 50, 235);
 
-		fill("#87dfd6");
-		ellipse(trees[i].tree_pos_x + 20, tree_pos_y, tree_pos_y, tree_pos_y - 50);
-	}
+    fill("#87dfd6");
+    ellipse(trees[i].tree_pos_x + 20, tree_pos_y, tree_pos_y, tree_pos_y - 50);
+  }
 }
 
 // ---------------------------------
@@ -39,21 +39,21 @@ function drawTrees() {
 //Function to draw canyon objects.
 
 function drawCanyon(t_canyon) {
-	fill("#f0ece3");
-	rect(t_canyon.x_pos, t_canyon.y_pos, t_canyon.width, t_canyon.width + 100);
+  fill("#f0ece3");
+  rect(t_canyon.x_pos, t_canyon.y_pos, t_canyon.width, t_canyon.width + 100);
 }
 //Function to check character whether a character is over a canyon.
 function checkCanyon(t_canyon) {
-	let distance;
-	distance = int(dist(t_canyon.x_pos, t_canyon.y_pos, game_char_world_x, game_char_y));
+  let distance;
+  distance = int(dist(t_canyon.x_pos, t_canyon.y_pos, game_char_world_x, game_char_y));
 
-	if (game_char_world_x > t_canyon.x_pos && game_char_world_x < t_canyon.x_pos + t_canyon.width && game_char_y >= floor_pos_y) {
-		is_plummeting = true;
-	}
+  if (game_char_world_x > t_canyon.x_pos && game_char_world_x < t_canyon.x_pos + t_canyon.width && game_char_y >= floor_pos_y) {
+    is_plummeting = true;
+  }
 
-	if (is_plummeting) {
-		game_char_y += 5;
-	}
+  if (is_plummeting) {
+    game_char_y += 5;
+  }
 }
 
 // ----------------------------------
@@ -63,166 +63,166 @@ function checkCanyon(t_canyon) {
 
 function drawCollectable(t_collectable) {
 
-	//collectible is on platform level
-	if (t_collectable.y_pos == platform_level) {
-		// Draw collectable items
-		stroke("#05dfd7");
-		strokeWeight(t_collectable.size + 15);
-		point(t_collectable.x_pos, t_collectable.y_pos);
+  //collectible is on platform level
+  if (t_collectable.y_pos == platform_level) {
+    // Draw collectable items
+    stroke("#05dfd7");
+    strokeWeight(t_collectable.size + 15);
+    point(t_collectable.x_pos, t_collectable.y_pos);
 
-		stroke(102, 0, 204);
-		strokeWeight(t_collectable.size);
-		point(t_collectable.x_pos, t_collectable.y_pos);
+    stroke(102, 0, 204);
+    strokeWeight(t_collectable.size);
+    point(t_collectable.x_pos, t_collectable.y_pos);
 
-		stroke("#a3f7bf");
-		strokeWeight(t_collectable.size + 10);
-		point(t_collectable.x_pos - 10, t_collectable.y_pos);
-		strokeWeight(5);
-		noStroke();
-		fill("#eaf6f6");
+    stroke("#a3f7bf");
+    strokeWeight(t_collectable.size + 10);
+    point(t_collectable.x_pos - 10, t_collectable.y_pos);
+    strokeWeight(5);
+    noStroke();
+    fill("#eaf6f6");
 
-	}
-	//platform is on ground level
-	else {
-		// Draw collectable items
-		stroke("#f76b8a");
-		strokeWeight(t_collectable.size + 15);
-		point(t_collectable.x_pos, t_collectable.y_pos);
+  }
+  //platform is on ground level
+  else {
+    // Draw collectable items
+    stroke("#f76b8a");
+    strokeWeight(t_collectable.size + 15);
+    point(t_collectable.x_pos, t_collectable.y_pos);
 
-		stroke(102, 0, 204);
-		strokeWeight(t_collectable.size);
-		point(t_collectable.x_pos, t_collectable.y_pos);
+    stroke(102, 0, 204);
+    strokeWeight(t_collectable.size);
+    point(t_collectable.x_pos, t_collectable.y_pos);
 
-		stroke("#fcfefe");
-		strokeWeight(t_collectable.size + 10);
-		point(t_collectable.x_pos - 10, t_collectable.y_pos);
-		strokeWeight(5);
-		noStroke();
-		fill("#eaf6f6");
-	}
+    stroke("#fcfefe");
+    strokeWeight(t_collectable.size + 10);
+    point(t_collectable.x_pos - 10, t_collectable.y_pos);
+    strokeWeight(5);
+    noStroke();
+    fill("#eaf6f6");
+  }
 }
 
 // Function to check character has collected an item.
 function checkCollectable(t_collectable) {
-	//declaration of range variable used
-	//within this function only 
-	let range;
+  //declaration of range variable used
+  //within this function only 
+  let range;
 
-	range = int(dist(t_collectable.x_pos, t_collectable.y_pos, game_char_world_x, game_char_y));
+  range = int(dist(t_collectable.x_pos, t_collectable.y_pos, game_char_world_x, game_char_y));
 
-	//if the collectible is within range and equals the y coordinate of the ground
-	//then the collectible has been found 
-	if (range < 48 && range > 10) {
-		t_collectable.is_found = true;
+  //if the collectible is within range and equals the y coordinate of the ground
+  //then the collectible has been found 
+  if (range < 48 && range > 10) {
+    t_collectable.is_found = true;
 
-		if (t_collectable.is_found && t_collectable.y_pos == floor_pos_y) {
-			game_score += 1;
-		}
+    if (t_collectable.is_found && t_collectable.y_pos == floor_pos_y) {
+      game_score += 1;
+    }
 
 
-		if (t_collectable.is_found && t_collectable.y_pos == platform_level) {
-			game_score += 2;
-		}
+    if (t_collectable.is_found && t_collectable.y_pos == platform_level) {
+      game_score += 2;
+    }
 
-	}
+  }
 }
 
 // ---------------------------------
 // End Flagpole render and check functions
 // ---------------------------------
 function renderEndFlagPole(sound) {
-	push();
-	strokeWeight(5);
-	stroke(0, 0, 0);
+  push();
+  strokeWeight(5);
+  stroke(0, 0, 0);
 
-	line(end_flagPole.x_pos, floor_pos_y, end_flagPole.x_pos, floor_pos_y - 250);
-	fill("#f3f9fb");
-	noStroke();
+  line(end_flagPole.x_pos, floor_pos_y, end_flagPole.x_pos, floor_pos_y - 250);
+  fill("#f3f9fb");
+  noStroke();
 
-	if (end_flagPole.is_reached) {
-		rect(end_flagPole.x_pos, floor_pos_y - 250, 50, 50);
-		sound.stop();
-	} else {
-		rect(end_flagPole.x_pos, floor_pos_y - 50, 50, 50);
-	}
+  if (end_flagPole.is_reached) {
+    rect(end_flagPole.x_pos, floor_pos_y - 250, 50, 50);
+    sound.stop();
+  } else {
+    rect(end_flagPole.x_pos, floor_pos_y - 50, 50, 50);
+  }
 
-	pop();
+  pop();
 }
 
 //check that the player reached the flagpole
 function checkEndFlagPole() {
-	let distance;
-	distance = abs(game_char_world_x - end_flagPole.x_pos);
+  let distance;
+  distance = abs(game_char_world_x - end_flagPole.x_pos);
 
-	if (distance < 5) {
-		end_flagPole.is_reached = true;
-	}
+  if (distance < 5) {
+    end_flagPole.is_reached = true;
+  }
 }
 
 // ---------------------------------
 // Begin flagpole and check functions
 // ---------------------------------
 function renderBeginFlagPole(sound) {
-	push();
-	strokeWeight(5);
-	stroke(0, 0, 0);
+  push();
+  strokeWeight(5);
+  stroke(0, 0, 0);
 
-	line(begin_flagPole.x_pos, floor_pos_y, begin_flagPole.x_pos, floor_pos_y - 250);
-	fill("#bae8e8");
-	noStroke();
+  line(begin_flagPole.x_pos, floor_pos_y, begin_flagPole.x_pos, floor_pos_y - 250);
+  fill("#bae8e8");
+  noStroke();
 
-	if (begin_flagPole.is_reached) {
-		rect(begin_flagPole.x_pos, floor_pos_y - 250, 50, 50);
-		sound.stop();
-	} else {
-		rect(begin_flagPole.x_pos, floor_pos_y - 50, 50, 50);
-	}
+  if (begin_flagPole.is_reached) {
+    rect(begin_flagPole.x_pos, floor_pos_y - 250, 50, 50);
+    sound.stop();
+  } else {
+    rect(begin_flagPole.x_pos, floor_pos_y - 50, 50, 50);
+  }
 
-	pop();
+  pop();
 }
 
 //check that the flagpole has been reached
 function beginFlagPoleReached() {
-	let distance;
-	distance = abs(game_char_world_x - begin_flagPole.x_pos);
+  let distance;
+  distance = abs(game_char_world_x - begin_flagPole.x_pos);
 
-	if (distance < 5) {
-		begin_flagPole.is_reached = true;
-	}
+  if (distance < 5) {
+    begin_flagPole.is_reached = true;
+  }
 }
 
 // ---------------------------------
 // Platform render function
 // ---------------------------------
 function createPlatforms(x, y, length, colour) {
-	var platform = {
-		x: x,
-		y: y,
-		length: length,
-		colour: colour,
-		draw: function () {
-			fill(this.colour);
+  var platform = {
+    x: x,
+    y: y,
+    length: length,
+    colour: colour,
+    draw: function() {
+      fill(this.colour);
 
-			//let the platforms shake a little if the 
-			//colour is a certain quality 
-			if (colour == "#2bb2bb") {
-				this.x = this.x + random(-0.5, 0.5);
-			}
+      //let the platforms shake a little if the 
+      //colour is a certain quality 
+      if (colour == "#2bb2bb") {
+        this.x = this.x + random(-0.5, 0.5);
+      }
 
-			rect(this.x, this.y, this.length, 50);
-		},
+      rect(this.x, this.y, this.length, 50);
+    },
 
-		checkContact: function (gc_x, gc_y) {
-			if (gc_x > this.x && gc_x < this.x + this.length) {
-				let platform_distance = this.y - gc_y;
-				if (platform_distance >= -2 && platform_distance < 6) {
-					return true;
-				}
-			}
-			return false;
-		}
-	}
-	return platform;
+    checkContact: function(gc_x, gc_y) {
+      if (gc_x > this.x && gc_x < this.x + this.length) {
+        let platform_distance = this.y - gc_y;
+        if (platform_distance >= -2 && platform_distance < 6) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+  return platform;
 }
 
 // ---------------------------------
@@ -230,23 +230,23 @@ function createPlatforms(x, y, length, colour) {
 // (code taken / modified from pj5 examples)
 // ---------------------------------
 function renderStar(x, y) {
-	let angle = TWO_PI / 5;
-	let half_angle = angle / 2.0;
+  let angle = TWO_PI / 5;
+  let half_angle = angle / 2.0;
 
-	noStroke();
-	fill("#f3ecc2");
+  noStroke();
+  fill("#f3ecc2");
 
-	beginShape();
-	for (var i = 0; i < TWO_PI; i += angle) {
+  beginShape();
+  for (var i = 0; i < TWO_PI; i += angle) {
 
-		let star_x = x + cos(i) * 70;
-		let star_y = y + sin(i) * 70;
+    let star_x = x + cos(i) * 70;
+    let star_y = y + sin(i) * 70;
 
-		vertex(star_x, star_y);
-		star_x = x + cos(i + half_angle) * 30;
-		star_y = y + sin(i + half_angle) * 30;
+    vertex(star_x, star_y);
+    star_x = x + cos(i + half_angle) * 30;
+    star_y = y + sin(i + half_angle) * 30;
 
-		vertex(star_x, star_y);
-	}
-	endShape(CLOSE);
+    vertex(star_x, star_y);
+  }
+  endShape(CLOSE);
 }
--- scripts/start.js
+++ scripts/start.js
@@ -46,503 +46,503 @@ var player_on_platform;
 var enemies;
 
 function startGame() {
-	createCanvas(1024, 576);
-	floor_pos_y = height * 3 / 4;
-	game_char_x = width / 2;
-	game_char_y = floor_pos_y;
+  createCanvas(1024, 576);
+  floor_pos_y = height * 3 / 4;
+  game_char_x = width / 2;
+  game_char_y = floor_pos_y;
 
-	/*
-	    Variable to store the real position of the gameChar in the game
-	    world. Needed for collision detection.
-	*/
-	game_char_world_x = game_char_x - scroll_position;
+  /*
+      Variable to store the real position of the gameChar in the game
+      world. Needed for collision detection.
+  */
+  game_char_world_x = game_char_x - scroll_position;
 
-	/*
-	    Variable to control the background scrolling.
-	*/
-	scroll_position = 0;
+  /*
+      Variable to control the background scrolling.
+  */
+  scroll_position = 0;
 
-	/*
-	    Boolean variables to control the
-	    movement of the game character
-	*/
-	is_left = false;
-	is_right = false;
-	is_on_platform = false;
+  /*
+      Boolean variables to control the
+      movement of the game character
+  */
+  is_left = false;
+  is_right = false;
+  is_on_platform = false;
 
-	is_falling = false;
-	is_plummeting = false;
-	is_on_platform = false;
+  is_falling = false;
+  is_plummeting = false;
+  is_on_platform = false;
 
-	/*
-	    initialise params that
-	    are used throughout the game
-	*/
-	canyons_width = 100;
-	tree_pos_y = 200;
-	game_score = 0;
+  /*
+      initialise params that
+      are used throughout the game
+  */
+  canyons_width = 100;
+  tree_pos_y = 200;
+  game_score = 0;
 
-	platform_level = floor_pos_y - 105;
-	player_on_platform = 332;
+  platform_level = floor_pos_y - 105;
+  player_on_platform = 332;
 
-	/*
-	    clouds array
-	*/
-	clouds = [{
-			x_pos: 250,
-			y_pos: 150,
-			width: 80,
-			height: 50
-		},
-		{
-			x_pos: 300,
-			y_pos: 150,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 350,
-			y_pos: 150,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 400,
-			y_pos: 150,
-			width: 100,
-			height: 50
-		},
-		{
-			x_pos: 700,
-			y_pos: 50,
-			width: 80,
-			height: 50
-		},
-		{
-			x_pos: 800,
-			y_pos: 50,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 850,
-			y_pos: 50,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 900,
-			y_pos: 50,
-			width: 100,
-			height: 50
-		},
-		{
-			x_pos: -500,
-			y_pos: 150,
-			width: 80,
-			height: 50
-		},
-		{
-			x_pos: -450,
-			y_pos: 150,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 250,
-			y_pos: 150,
-			width: 80,
-			height: 50
-		},
-		{
-			x_pos: 1500,
-			y_pos: 50,
-			width: 100,
-			height: 50
-		},
-		{
-			x_pos: 1550,
-			y_pos: 50,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 1600,
-			y_pos: 50,
-			width: 100,
-			height: 50
-		},
-		{
-			x_pos: 2200,
-			y_pos: 50,
-			width: 100,
-			height: 50
-		},
-		{
-			x_pos: 2250,
-			y_pos: 50,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: 2300,
-			y_pos: 50,
-			width: 100,
-			height: 50
-		},
-		{
-			x_pos: 3600,
-			y_pos: 150,
-			width: 100,
-			height: 80
-		},
-		{
-			x_pos: 3650,
-			y_pos: 150,
-			width: 120,
-			height: 100
-		},
-		{
-			x_pos: 3700,
-			y_pos: 150,
-			width: 100,
-			height: 80
-		},
-		{
-			x_pos: -1550,
-			y_pos: 150,
-			width: 80,
-			height: 50
-		},
-		{
-			x_pos: -1600,
-			y_pos: 150,
-			width: 100,
-			height: 75
-		},
-		{
-			x_pos: -1650,
-			y_pos: 150,
-			width: 100,
-			height: 75
-		}
-	];
+  /*
+      clouds array
+  */
+  clouds = [{
+      x_pos: 250,
+      y_pos: 150,
+      width: 80,
+      height: 50
+    },
+    {
+      x_pos: 300,
+      y_pos: 150,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 350,
+      y_pos: 150,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 400,
+      y_pos: 150,
+      width: 100,
+      height: 50
+    },
+    {
+      x_pos: 700,
+      y_pos: 50,
+      width: 80,
+      height: 50
+    },
+    {
+      x_pos: 800,
+      y_pos: 50,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 850,
+      y_pos: 50,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 900,
+      y_pos: 50,
+      width: 100,
+      height: 50
+    },
+    {
+      x_pos: -500,
+      y_pos: 150,
+      width: 80,
+      height: 50
+    },
+    {
+      x_pos: -450,
+      y_pos: 150,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 250,
+      y_pos: 150,
+      width: 80,
+      height: 50
+    },
+    {
+      x_pos: 1500,
+      y_pos: 50,
+      width: 100,
+      height: 50
+    },
+    {
+      x_pos: 1550,
+      y_pos: 50,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 1600,
+      y_pos: 50,
+      width: 100,
+      height: 50
+    },
+    {
+      x_pos: 2200,
+      y_pos: 50,
+      width: 100,
+      height: 50
+    },
+    {
+      x_pos: 2250,
+      y_pos: 50,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: 2300,
+      y_pos: 50,
+      width: 100,
+      height: 50
+    },
+    {
+      x_pos: 3600,
+      y_pos: 150,
+      width: 100,
+      height: 80
+    },
+    {
+      x_pos: 3650,
+      y_pos: 150,
+      width: 120,
+      height: 100
+    },
+    {
+      x_pos: 3700,
+      y_pos: 150,
+      width: 100,
+      height: 80
+    },
+    {
+      x_pos: -1550,
+      y_pos: 150,
+      width: 80,
+      height: 50
+    },
+    {
+      x_pos: -1600,
+      y_pos: 150,
+      width: 100,
+      height: 75
+    },
+    {
+      x_pos: -1650,
+      y_pos: 150,
+      width: 100,
+      height: 75
+    }
+  ];
 
-	/*
-	    mountains array
-	*/
-	mountains = [{
-			x_pos: 500,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 100,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 600,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 850,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 1000,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: -800,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 2500,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 2200,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 1400,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 2800,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: 3900,
-			y_pos: floor_pos_y
-		},
-		{
-			x_pos: -1600,
-			y_pos: floor_pos_y
-		}
+  /*
+      mountains array
+  */
+  mountains = [{
+      x_pos: 500,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 100,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 600,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 850,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 1000,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: -800,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 2500,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 2200,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 1400,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 2800,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: 3900,
+      y_pos: floor_pos_y
+    },
+    {
+      x_pos: -1600,
+      y_pos: floor_pos_y
+    }
 
-	];
+  ];
 
-	/*
-	    trees array
-	*/
-	trees = [{
-			tree_pos_x: 45
-		},
-		{
-			tree_pos_x: -193
-		},
-		{
-			tree_pos_x: 750
-		},
-		{
-			tree_pos_x: 1747
-		},
-		{
-			tree_pos_x: 2072
-		},
-		{
-			tree_pos_x: -745
-		},
-		{
-			tree_pos_x: 2800
-		},
-		{
-			tree_pos_x: 3300
-		},
-		{
-			tree_pos_x: -1800
-		}
-	];
+  /*
+      trees array
+  */
+  trees = [{
+      tree_pos_x: 45
+    },
+    {
+      tree_pos_x: -193
+    },
+    {
+      tree_pos_x: 750
+    },
+    {
+      tree_pos_x: 1747
+    },
+    {
+      tree_pos_x: 2072
+    },
+    {
+      tree_pos_x: -745
+    },
+    {
+      tree_pos_x: 2800
+    },
+    {
+      tree_pos_x: 3300
+    },
+    {
+      tree_pos_x: -1800
+    }
+  ];
 
-	/*
-	    canyons array
-	*/
-	canyons = [{
-			x_pos: 550,
-			y_pos: floor_pos_y,
-			width: canyons_width
-		},
-		{
-			x_pos: 320,
-			y_pos: floor_pos_y,
-			width: canyons_width
-		},
-		{
-			x_pos: 1400,
-			y_pos: floor_pos_y,
-			width: canyons_width
-		},
-		{
-			x_pos: 1900,
-			y_pos: floor_pos_y,
-			width: canyons_width
-		},
-		{
-			x_pos: -550,
-			y_pos: floor_pos_y,
-			width: canyons_width
-		},
-		{
-			x_pos: 3500,
-			y_pos: floor_pos_y,
-			width: canyons_width
-		}
-	];
-
-
-	/*
-	   stars
-	*/
-	stars = [{
-			x_pos: -550,
-			y_pos: platform_level
-		},
-		{
-			x_pos: 100,
-			y_pos: platform_level
-		},
-		{
-			x_pos: 804,
-			y_pos: platform_level
-		},
-		{
-			x_pos: 1600,
-			y_pos: platform_level
-		},
-		{
-			x_pos: 1200,
-			y_pos: platform_level
-		},
-		{
-			x_pos: -850,
-			y_pos: platform_level
-		},
-		{
-			x_pos: -1800,
-			y_pos: platform_level
-		}
-	];
-
-	/*
-	    flagpoles
-	*/
-
-	//flagpole at the 
-	//end of the game (to the right)
-	end_flagPole = {
-		x_pos: 2100,
-		is_reached: false
-	};
+  /*
+      canyons array
+  */
+  canyons = [{
+      x_pos: 550,
+      y_pos: floor_pos_y,
+      width: canyons_width
+    },
+    {
+      x_pos: 320,
+      y_pos: floor_pos_y,
+      width: canyons_width
+    },
+    {
+      x_pos: 1400,
+      y_pos: floor_pos_y,
+      width: canyons_width
+    },
+    {
+      x_pos: 1900,
+      y_pos: floor_pos_y,
+      width: canyons_width
+    },
+    {
+      x_pos: -550,
+      y_pos: floor_pos_y,
+      width: canyons_width
+    },
+    {
+      x_pos: 3500,
+      y_pos: floor_pos_y,
+      width: canyons_width
+    }
+  ];
 
 
-	//flagpole at the 
-	//end of the game (to the left)
-	begin_flagPole = {
+  /*
+     stars
+  */
+  stars = [{
+      x_pos: -550,
+      y_pos: platform_level
+    },
+    {
+      x_pos: 100,
+      y_pos: platform_level
+    },
+    {
+      x_pos: 804,
+      y_pos: platform_level
+    },
+    {
+      x_pos: 1600,
+      y_pos: platform_level
+    },
+    {
+      x_pos: 1200,
+      y_pos: platform_level
+    },
+    {
+      x_pos: -850,
+      y_pos: platform_level
+    },
+    {
+      x_pos: -1800,
+      y_pos: platform_level
+    }
+  ];
 
-		x_pos: -2000,
-		is_reached: false
-	};
+  /*
+      flagpoles
+  */
 
-	/*
-	    enemies initialisation
-	*/
-	enemies = [];
-
-	//enemies on platform level
-	enemies.push(new Enemy(100, platform_level, 100));
-	enemies.push(new Enemy(750, platform_level, 100));
-	enemies.push(new Enemy(-560, platform_level, 85));
-	enemies.push(new Enemy(1500, platform_level, 85));
-
-	enemies.push(new Enemy(1200, platform_level, 50));
-	enemies.push(new Enemy(-1800, platform_level, 50));
-	enemies.push(new Enemy(-850, platform_level, 85));
+  //flagpole at the 
+  //end of the game (to the right)
+  end_flagPole = {
+    x_pos: 2100,
+    is_reached: false
+  };
 
 
-	//enemies on the ground 
-	enemies.push(new Enemy(1600, floor_pos_y - 10, 300));
-	enemies.push(new Enemy(-200, floor_pos_y - 10, 300));
-	enemies.push(new Enemy(-1200, floor_pos_y - 10, 250));
+  //flagpole at the 
+  //end of the game (to the left)
+  begin_flagPole = {
 
-	/*
-	    collectibles array
-	*/
+    x_pos: -2000,
+    is_reached: false
+  };
 
-	collectibles = [{
-			x_pos: 202,
-			y_pos: platform_level,
-			size: 5,
-			is_found: false
-		},
-		{
-			x_pos: 750,
-			y_pos: platform_level,
-			size: 2,
-			is_found: false
-		},
-		{
-			x_pos: 110,
-			y_pos: platform_level,
-			size: 3,
-			is_found: false
-		},
-		{
-			x_pos: 880,
-			y_pos: platform_level,
-			size: 2,
-			is_found: false
-		},
-		{
-			x_pos: 1272,
-			y_pos: platform_level,
-			size: 3,
-			is_found: false
-		},
-		{
-			x_pos: 1502,
-			y_pos: platform_level,
-			size: 2,
-			is_found: false
-		},
-		{
-			x_pos: -1673,
-			y_pos: platform_level,
-			size: 5,
-			is_found: false
-		},
-		{
-			x_pos: -418,
-			y_pos: platform_level,
-			size: 3,
-			is_found: false
-		},
-		{
-			x_pos: -763,
-			y_pos: platform_level,
-			size: 4,
-			is_found: false
-		},
-		{
-			x_pos: -1000,
-			y_pos: floor_pos_y,
-			size: 2,
-			is_found: false
-		},
-		{
-			x_pos: 1600,
-			y_pos: floor_pos_y,
-			size: 5,
-			is_found: false
-		},
-		{
-			x_pos: 1900,
-			y_pos: floor_pos_y,
-			size: 5,
-			is_found: false
-		},
-		{
-			x_pos: 1100,
-			y_pos: floor_pos_y,
-			size: 2,
-			is_found: false
-		},
-		{
-			x_pos: 200,
-			y_pos: floor_pos_y,
-			size: 5,
-			is_found: false
-		},
-		{
-			x_pos: 330,
-			y_pos: floor_pos_y,
-			size: 3,
-			is_found: false
-		},
-		{
-			x_pos: 450,
-			y_pos: floor_pos_y,
-			size: 5,
-			is_found: false
-		}
-	];
+  /*
+      enemies initialisation
+  */
+  enemies = [];
 
-	/*
-	    platforms
-	*/
+  //enemies on platform level
+  enemies.push(new Enemy(100, platform_level, 100));
+  enemies.push(new Enemy(750, platform_level, 100));
+  enemies.push(new Enemy(-560, platform_level, 85));
+  enemies.push(new Enemy(1500, platform_level, 85));
 
-	let platform_y = floor_pos_y - 95;
-	let platform_length = 150;
+  enemies.push(new Enemy(1200, platform_level, 50));
+  enemies.push(new Enemy(-1800, platform_level, 50));
+  enemies.push(new Enemy(-850, platform_level, 85));
 
-	platforms = [];
 
-	//platforms alternate between two colours
-	platforms.push(createPlatforms(100, platform_y, platform_length, "#2bb2bb"));
-	platforms.push(createPlatforms(750, platform_y, platform_length, "#87d4c5"));
-	platforms.push(createPlatforms(1200, platform_y, platform_length, "#2bb2bb"));
-	platforms.push(createPlatforms(1500, platform_y, platform_length, "#87d4c5"));
+  //enemies on the ground 
+  enemies.push(new Enemy(1600, floor_pos_y - 10, 300));
+  enemies.push(new Enemy(-200, floor_pos_y - 10, 300));
+  enemies.push(new Enemy(-1200, floor_pos_y - 10, 250));
 
-	platforms.push(createPlatforms(-550, platform_y, platform_length, "#2bb2bb"));
-	platforms.push(createPlatforms(-850, platform_y, platform_length, "#87d4c5"));
-	platforms.push(createPlatforms(-1800, platform_y, platform_length, "#2bb2bb"));
+  /*
+      collectibles array
+  */
+
+  collectibles = [{
+      x_pos: 202,
+      y_pos: platform_level,
+      size: 5,
+      is_found: false
+    },
+    {
+      x_pos: 750,
+      y_pos: platform_level,
+      size: 2,
+      is_found: false
+    },
+    {
+      x_pos: 110,
+      y_pos: platform_level,
+      size: 3,
+      is_found: false
+    },
+    {
+      x_pos: 880,
+      y_pos: platform_level,
+      size: 2,
+      is_found: false
+    },
+    {
+      x_pos: 1272,
+      y_pos: platform_level,
+      size: 3,
+      is_found: false
+    },
+    {
+      x_pos: 1502,
+      y_pos: platform_level,
+      size: 2,
+      is_found: false
+    },
+    {
+      x_pos: -1673,
+      y_pos: platform_level,
+      size: 5,
+      is_found: false
+    },
+    {
+      x_pos: -418,
+      y_pos: platform_level,
+      size: 3,
+      is_found: false
+    },
+    {
+      x_pos: -763,
+      y_pos: platform_level,
+      size: 4,
+      is_found: false
+    },
+    {
+      x_pos: -1000,
+      y_pos: floor_pos_y,
+      size: 2,
+      is_found: false
+    },
+    {
+      x_pos: 1600,
+      y_pos: floor_pos_y,
+      size: 5,
+      is_found: false
+    },
+    {
+      x_pos: 1900,
+      y_pos: floor_pos_y,
+      size: 5,
+      is_found: false
+    },
+    {
+      x_pos: 1100,
+      y_pos: floor_pos_y,
+      size: 2,
+      is_found: false
+    },
+    {
+      x_pos: 200,
+      y_pos: floor_pos_y,
+      size: 5,
+      is_found: false
+    },
+    {
+      x_pos: 330,
+      y_pos: floor_pos_y,
+      size: 3,
+      is_found: false
+    },
+    {
+      x_pos: 450,
+      y_pos: floor_pos_y,
+      size: 5,
+      is_found: false
+    }
+  ];
+
+  /*
+      platforms
+  */
+
+  let platform_y = floor_pos_y - 95;
+  let platform_length = 150;
+
+  platforms = [];
+
+  //platforms alternate between two colours
+  platforms.push(createPlatforms(100, platform_y, platform_length, "#2bb2bb"));
+  platforms.push(createPlatforms(750, platform_y, platform_length, "#87d4c5"));
+  platforms.push(createPlatforms(1200, platform_y, platform_length, "#2bb2bb"));
+  platforms.push(createPlatforms(1500, platform_y, platform_length, "#87d4c5"));
+
+  platforms.push(createPlatforms(-550, platform_y, platform_length, "#2bb2bb"));
+  platforms.push(createPlatforms(-850, platform_y, platform_length, "#87d4c5"));
+  platforms.push(createPlatforms(-1800, platform_y, platform_length, "#2bb2bb"));
 
 }
